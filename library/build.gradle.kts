aad18b61488b1f23de07925dd19da9f4bed760f9
Ahmad Ansori Palembani <palembani@gmail.com>
2024-09-20 10:34:19 +0700

    chore(gradle): POM

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -10,6 +10,19 @@ dependencies {
 publishing {
     publications {
         create<MavenPublication>("maven") {
+            pom {
+                name = "Injekt-Koin"
+                description = "Bridge between Injekt and Koin"
+                url = "https://github.com/null2264/injekt-koin"
+
+                licenses {
+                    license {
+                        name = "MIT"
+                        url = "https://github.com/null2264/injekt-koin/blob/master/LICENSE"
+                        distribution = "repo"
+                    }
+                }
+            }
             from(components["kotlin"])
         }
     }
