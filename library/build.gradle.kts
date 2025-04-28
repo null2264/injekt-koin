776c71d2c632454e611fac0c81bdeebe97332954
Ahmad Ansori Palembani <palembani@gmail.com>
2025-04-28 14:44:06 +0700

    chore: Maven publishing

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -7,9 +7,16 @@ dependencies {
     implementation(libs.koin)
 }
 
+val ENV = System.getenv()
+
+group = "io.github.null2264"
+
 publishing {
     publications {
         create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "injekt-koin"
+            version = "1.0-SNAPSHOT"
             pom {
                 name = "Injekt-Koin"
                 description = "Bridge between Injekt and Koin"
@@ -24,6 +31,18 @@ publishing {
                 }
             }
             from(components["kotlin"])
+        }
+    }
+
+    repositories {
+        if (ENV["MAVEN_URL"] != null) {
+            maven {
+                url = uri(ENV["MAVEN_URL"] ?: "")
+                credentials {
+                    username = ENV["MAVEN_USER"]
+                    password = ENV["MAVEN_PASS"]
+                }
+            }
         }
     }
 }
